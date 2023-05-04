2e2df10f854a5e111daad15e535771a02540b242
cavansir112 <coni.muradov@bk.ru>
2023-05-04 16:52:04 +0300

    java exercise1

---
+++ MyCode.java
@@ -0,0 +1,41 @@
+public class MyCode{
+
+	public static void main(String[] args){
+		
+		for(int i = 1; i < 5; i++){
+			
+			System.out.println(i);
+			
+		};
+		
+		int num1 = 5;
+		int num2 = 7;
+		System.out.println(num1 + num2);
+		System.out.println(num1 * num2);
+		
+		for(int val = 50; val > 20; val--){
+			
+			System.out.println(val);
+			
+		};
+		
+		for(int val2 = 1; val2 < 200; val2++){
+			
+			if(val2%7==0 & val2%3==0){
+				
+				System.out.println(val2);
+			}
+						
+			
+		};
+		
+		for(int val3 = 0.1; val3 < 5; val3++){
+			
+			System.out.println(val3);
+			
+		}
+	
+	
+	}
+
+}
